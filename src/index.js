7c61092dabdef411b43099f256c89013dcb4d803
Tony Sheng <Tony@Tonys-MacBook-Pro.local>
2019-10-21 09:29:33 -0400

    Commit to push to live

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './styles/index.scss';
-import {updateMap} from './update';
-import bubbleSort from './sort/bubblesort';
 import "@babel/polyfill";
-import renderSwaps from './sort/quicksort';
 import Map from './create_map';
-import QuickSortClass from './sort/quicksort';
 
 window.addEventListener('DOMContentLoaded', () => {
 
